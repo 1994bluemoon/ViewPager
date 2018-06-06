d8e8bf91dc9c6671a564861f284018c6b7b3191e
Henry Duong <minhhoang94_bluemoon@yahoo.com.vn>
2018-06-06 12:47:39 +0700

    Initial commit

---
+++ app/src/main/java/com/journaldev/viewpager/MainActivity.java
@@ -0,0 +1,20 @@
+package com.journaldev.viewpager;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        viewPager.setAdapter(new CustomPagerAdapter(this));
+
+    }
+
+
+}
